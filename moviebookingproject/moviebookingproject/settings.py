b811eb9edce88414e2d9a85ef92bd2830f79692e
Vivek Mondal <vivekmondal9@gmail.com>
2023-08-29 16:26:43 +0530

    allowing cors origin to all

--- moviebookingproject/moviebookingproject/settings.py
+++ moviebookingproject/moviebookingproject/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS=True
+
 CORS_ORIGIN_WHITELIST=[
     'http://localhost:3000',
     # 'https://another-example.com'
